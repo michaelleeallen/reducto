2e20cff502c258e3435883d4155fce1a5bc84cbc
Michael Allen <michaelallen@Michael-Allens-MacBook-Pro-15.local>
2014-06-12 20:57:04 -0400

    Fixes #4 by adding transform middleware to routes. Also, code cleanup.

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 var _ = require('underscore');
+var utils = require('./lib/utils');
 var middleware = require('./lib/middleware');
 var fixture = require('./lib/fixture');
 var service = require('./lib/service');
-
-/** 
- * Takes a config object and a list of functions to
- * configure middleware and push onto the stack.
- * @param {object} config - current route config
- * @param {function,?} n number of configuration functions
- * @returns {array} a list of middleware functions
- */
-var loadStack = function(config){
-  var args = _.rest(_.toArray(arguments));
-  var stack = [];
-  args.forEach(function(fn){
-    var out = fn(config);
-    if ( out ) {
-      if ( _.isArray(out) ){
-        stack = stack.concat(out);
-      } else {
-        stack.push(out);
-      }
-    }
-  });
-  return stack;
-};
+var transform = require('./lib/transform');
 
 /**
  * Reads in route and service definitions from JSON and configures
@@ -34,11 +13,12 @@ var loadStack = function(config){
 module.exports = function(app, routes, services){
   _.each(routes, function(methods, route){
     _.each(methods, function(config, method){
-      app[method](route, loadStack(
+      app[method](route, utils.loadStack(
         config, 
         middleware,
         fixture, 
-        service.bind(null, services)
+        service.bind(null, services),
+        transform
       ));
     });
   });
