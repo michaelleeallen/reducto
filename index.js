20a3bcda74e4e054a493c956c30a98fdaf20f337
Michael Allen <michaelallen@Michael-Allens-MacBook-Pro-15.local>
2014-06-12 21:12:12 -0400

    Revert "Fixes #4 by adding transform middleware to routes. Also, code cleanup."
    This reverts commit 2e20cff502c258e3435883d4155fce1a5bc84cbc.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 var _ = require('underscore');
-var utils = require('./lib/utils');
 var middleware = require('./lib/middleware');
 var fixture = require('./lib/fixture');
 var service = require('./lib/service');
-var transform = require('./lib/transform');
+
+/** 
+ * Takes a config object and a list of functions to
+ * configure middleware and push onto the stack.
+ * @param {object} config - current route config
+ * @param {function,?} n number of configuration functions
+ * @returns {array} a list of middleware functions
+ */
+var loadStack = function(config){
+  var args = _.rest(_.toArray(arguments));
+  var stack = [];
+  args.forEach(function(fn){
+    var out = fn(config);
+    if ( out ) {
+      if ( _.isArray(out) ){
+        stack = stack.concat(out);
+      } else {
+        stack.push(out);
+      }
+    }
+  });
+  return stack;
+};
 
 /**
  * Reads in route and service definitions from JSON and configures
@@ -13,12 +34,11 @@ var transform = require('./lib/transform');
 module.exports = function(app, routes, services){
   _.each(routes, function(methods, route){
     _.each(methods, function(config, method){
-      app[method](route, utils.loadStack(
+      app[method](route, loadStack(
         config, 
         middleware,
         fixture, 
-        service.bind(null, services),
-        transform
+        service.bind(null, services)
       ));
     });
   });
