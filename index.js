fdb94611b4441471bb18826e267f72c4dc2e9a7c
Michael Allen <Michael.Allen@benefitfocus.com>
2014-07-04 15:47:44 -0400

    Fixes #7 by adding "viewName" config option to routes, update example app to show view rendering, add view-spec, update README to include "viewName" config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var middleware = require('./lib/middleware');
 var fixture = require('./lib/fixture');
 var service = require('./lib/service');
 var transform = require('./lib/transform');
-
+var view = require('./lib/view');
 /**
  * Reads in route and service definitions from JSON and configures
  * express routes with the appropriate middleware for each.
@@ -14,11 +14,12 @@ module.exports = function(app, routes, services){
   _.each(routes, function(methods, route){
     _.each(methods, function(config, method){
       app[method](route, utils.loadStack(
-        config, 
+        config,
         middleware,
-        fixture, 
+        fixture,
         service.bind(null, services),
-        transform
+        transform,
+        view
       ));
     });
   });
